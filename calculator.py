589ce6b014531abf7bd0ba3286a90f17703bb9c1
jm1128825-arch <jm1128825@gmail.com>
2026-07-14 13:30:37 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,14 @@
 def solve(expression):
-    expression = expression.replace("×", "*").replace("÷", "/")
+    expression = (
+        expression
+        .replace("×", "*")
+        .replace("÷", "/")
+        .replace("%", "/100")
+    )
 
-    return str(eval(expression))
+    result = eval(expression)
+
+    if isinstance(result, float) and result.is_integer():
+        result = int(result)
+
+    return str(result)
