5f629ace3a6dd847dbb261f5879b7932782d068c
jm1128825-arch <jm1128825@gmail.com>
2026-07-14 13:20:57 -0700

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,32 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def calculator_keyboard():
+    keyboard = [
+        [
+            InlineKeyboardButton("7", callback_data="7"),
+            InlineKeyboardButton("8", callback_data="8"),
+            InlineKeyboardButton("9", callback_data="9"),
+            InlineKeyboardButton("÷", callback_data="/"),
+        ],
+        [
+            InlineKeyboardButton("4", callback_data="4"),
+            InlineKeyboardButton("5", callback_data="5"),
+            InlineKeyboardButton("6", callback_data="6"),
+            InlineKeyboardButton("×", callback_data="*"),
+        ],
+        [
+            InlineKeyboardButton("1", callback_data="1"),
+            InlineKeyboardButton("2", callback_data="2"),
+            InlineKeyboardButton("3", callback_data="3"),
+            InlineKeyboardButton("−", callback_data="-"),
+        ],
+        [
+            InlineKeyboardButton("C", callback_data="clear"),
+            InlineKeyboardButton("0", callback_data="0"),
+            InlineKeyboardButton("=", callback_data="equals"),
+            InlineKeyboardButton("+", callback_data="+"),
+        ],
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
